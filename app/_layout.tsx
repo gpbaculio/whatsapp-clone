9ab756c5b7163c174df6831de05f07ac4fa3e3ba
Glendon Philipp Baculio <gpbaculio@gmail.com>
2024-05-28 05:18:38 +0800

    remove unused

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,11 +10,6 @@ export {
   ErrorBoundary,
 } from "expo-router";
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: "(tabs)",
-};
-
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
@@ -45,8 +40,7 @@ export default function RootLayout() {
 function RootLayoutNav() {
   return (
     <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const index = () => {
+  return (
+    <View>
+      <Text>Phil Dominic 3year Baculio</Text>
+    </View>
+  );
+};
+
+export default index;
