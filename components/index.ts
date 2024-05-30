db4cc806a059a81d855d23ee2f840130718913f0
Glendon Philipp Baculio <gpbaculio@gmail.com>
2024-05-30 13:20:11 +0800

    Reusable components

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,33 @@
-import { createText } from "@shopify/restyle";
+import { Pressable, TouchableOpacity, View } from "react-native";
 
-import DynamicView from "./DynamicView";
+import {
+  createRestyleComponent,
+  createText,
+  createVariant,
+  VariantProps,
+} from "@shopify/restyle";
 
 import { Theme } from "@/app/theme";
+import Animated from "react-native-reanimated";
+
+const createContainer = (Component: React.ComponentType<any>) => {
+  return createRestyleComponent<
+    VariantProps<Theme, "containerVariants"> &
+      React.ComponentProps<typeof Component>,
+    Theme
+  >([createVariant({ themeKey: "containerVariants" })], Component);
+};
 
 const DynamicText = createText<Theme>();
+const DynamicTouchableOpacity = createContainer(TouchableOpacity);
+const DynamicPressable = createContainer(Pressable);
+const DynamicView = createContainer(View);
+const DynamicAnimatedView = Animated.createAnimatedComponent(DynamicView);
 
-export { DynamicView, DynamicText };
+export {
+  DynamicAnimatedView,
+  DynamicView,
+  DynamicText,
+  DynamicPressable,
+  DynamicTouchableOpacity,
+};
