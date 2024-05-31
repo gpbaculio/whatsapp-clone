e239d78c68c8ff8860d83138d46e1eff6d2ad9de
Glendon Philipp Baculio <gpbaculio@gmail.com>
2024-06-01 03:14:35 +0800

    Add Clerk Config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,9 @@ import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
 import theme from "./theme";
+import { ClerkProvider, useAuth } from "@clerk/clerk-expo";
+const CLERK_PUBLISHABLE_KEY = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY;
+import * as SecureStore from "expo-secure-store";
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -15,22 +18,25 @@ export {
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
-function RootLayoutNav() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="otp"
-        options={{
-          headerTitle: "Enter your Phone number",
-          headerBackVisible: false,
-        }}
-      />
-    </Stack>
-  );
-}
+// Cache the Clerk JWT
+const tokenCache = {
+  async getToken(key: string) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key: string, value: string) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
 
-export default function RootLayout() {
+function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     ...FontAwesome.font,
@@ -53,7 +59,33 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider theme={theme}>
-      <RootLayoutNav />
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="otp"
+          options={{
+            headerTitle: "Enter your Phone number",
+            headerBackVisible: false,
+          }}
+        />
+        <Stack.Screen
+          name="verify/[phone]"
+          options={{
+            headerBackTitle: "Edit number",
+          }}
+        />
+      </Stack>
     </ThemeProvider>
+  );
+}
+
+export default function RootLayoutNav() {
+  return (
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY!}
+      tokenCache={tokenCache}
+    >
+      <RootLayout />
+    </ClerkProvider>
   );
 }
