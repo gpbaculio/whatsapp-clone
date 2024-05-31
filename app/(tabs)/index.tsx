a8223835ae5ce2f36abddd1875805804d2fc2084
Glendon Philipp Baculio <gpbaculio@gmail.com>
2024-06-01 03:14:24 +0800

    Create index.tsx

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const PageIndex = () => {
+  return (
+    <View>
+      <Text>PageIndex</Text>
+    </View>
+  );
+};
+
+export default PageIndex;
